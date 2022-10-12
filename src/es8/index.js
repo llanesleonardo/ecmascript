d1df22a3629c3c8f1195c3b795bac3c1678395c6
llanesleonardo <llanesleonardo@gmail.com>
2022-10-12 16:20:47 -0700

    tema 8 async await y try catch - es8 Junio 2017

--- src/es8/index.js
+++ src/es8/index.js
@@ -34,3 +34,31 @@ console.log(string.padEnd(12, " ----"));
 const data2 = {
   name: "Leonardo",
 };
+
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    false
+      ? setTimeout(() => resolve("Hello World"), 3000)
+      : reject(new Error("Test error"));
+  });
+};
+
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  const hello2 = await helloWorld();
+  console.log(hello);
+  console.log(hello2);
+};
+
+helloAsync();
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+anotherFunction();
