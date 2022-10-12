53435f127cb01d2e14157600e97cd933b17cc4d8
llanesleonardo <llanesleonardo@gmail.com>
2022-10-12 12:20:57 -0700

    tema 2 spread operator, destructuring,saltos de linea con template literals y constante con conts, scope con let y var

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,4 +1,4 @@
-// old way
+// old way definición de valores por default
 function newFuction(name, age, country) {
   var name = name | "Leonardo";
   var age = age | 32;
@@ -6,7 +6,7 @@ function newFuction(name, age, country) {
   console.log(name, age, country);
 }
 
-//es6
+//es6 definición de valores por default
 
 function newFunction2(name = "Leonardo", age = 32, country = "MX") {
   console.log(name, age, country);
@@ -15,7 +15,7 @@ function newFunction2(name = "Leonardo", age = 32, country = "MX") {
 newFunction2();
 newFunction2("Cecilia", "37", "USA");
 
-//old way
+//old way concatenación
 
 let hello = "Hello";
 let world = "World";
@@ -23,7 +23,64 @@ let world = "World";
 let epicPhrase = hello + " " + world;
 console.log(epicPhrase);
 
-//template literals
+//es6 concatenación template literals
 
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+// old way salto de linea
+let lorem =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n " +
+  "otra frase epica que necesitamos";
+
+// es6  salto de linea template literals
+let lorem2 = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+
+  ahora es otra frase epica
+  `;
+
+console.log(lorem);
+
+console.log(lorem2);
+
+// old way destructuración
+let person = {
+  nombre: "Leonardo",
+  age: 32,
+  country: "MX",
+};
+
+console.log(person.name, person.age);
+
+// es6 destructuración
+
+let { name, age, country } = person;
+console.log(name, age, country);
+
+//es6 agregar otros arreglos a un nuevo arreglo Spread operator
+
+let team1 = ["Leonardo", "Julian", "Ricardo"];
+let team2 = ["Valeria", "Jessica", "Camila"];
+
+let education = ["David", ...team1, ...team2];
+
+console.log(team1);
+console.log(team2);
+console.log(education);
+
+{
+  var globalVar = "Global Var";
+}
+
+{
+  let globalLet = "Global let";
+  console.log(globalLet);
+}
+
+console.log(globalVar);
+
+//const Constantes
+const a = "b";
+a = "a";
+console.log(a);
+// su impresión es un error TypeError: Assignment to constant variable.
